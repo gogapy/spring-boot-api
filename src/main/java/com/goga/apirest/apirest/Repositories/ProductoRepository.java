8ebab2760164cd50c08591fd844aa18aa0af1e7f
Joaco <joaquinnogallar@gmail.com>
2024-07-02 15:18:53 -0300

    api rest spring boot, hibernate, docker y postgresql

---
+++ src/main/java/com/goga/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.goga.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.goga.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+        
+} 
