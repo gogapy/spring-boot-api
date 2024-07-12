71c70a348bad476e9353fcbf65eca080ee301aa3
Joaco <joaquinnogallar@gmail.com>
2024-07-12 00:45:24 -0300

    add: vista al proyecto utilizando thymeleaf

--- src/main/java/com/goga/apirest/apirest/Controllers/ProductoController.java
+++ src/main/java/com/goga/apirest/apirest/Controllers/ProductoController.java
@@ -3,53 +3,77 @@ package com.goga.apirest.apirest.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.goga.apirest.apirest.Entities.Producto;
 import com.goga.apirest.apirest.Repositories.ProductoRepository;
 
-@RestController
+// cambiamos @RestController por @Controller para poder manejar vistas Thymeleaf
+@Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
     @Autowired
     private ProductoRepository productoRepository;
 
-    @GetMapping
-    public List<Producto> getAllProductos() {
-        return productoRepository.findAll();
+    @GetMapping()
+    public String getAllProductos(Model model) {
+        model.addAttribute("productos", productoRepository.findAll());
+        return "productos";
     }
 
     @GetMapping("/{id}")
-    public Producto getProductoById(@PathVariable Long id) {
-        return productoRepository.findById(id).orElseThrow(() -> new RuntimeException("No se encontro el producto con el id: " + id));
+    public String getProductoById(@PathVariable Long id, Model model) {
+        model.addAttribute("producto", productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontro el producto con el id: " + id)));
+        return "infoProducto";
+    }
+
+    @GetMapping("/agregar")
+    public String getFormulario(Model model) {
+        model.addAttribute("producto", new Producto());
+        return "agregarProducto";
     }
 
     @PostMapping
-    public Producto createProducto(@RequestBody Producto producto) {
-        return productoRepository.save(producto);
+    public String createProducto(@ModelAttribute Producto producto) {
+        productoRepository.save(producto);
+        return "redirect:/productos";
     }
 
-    @PutMapping("/{id}")
-    public Producto updateProducto(@PathVariable Long id, @RequestBody Producto detallesProducto) {
-        Producto producto = productoRepository.findById(id).orElseThrow(() -> new RuntimeException("No se encontro el producto con el id: " + id));
+    @GetMapping("/update/{id}")
+    public String getFormularioUpdate(@PathVariable Long id, Model model) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontro el producto con el id: " + id));
+        model.addAttribute("producto", producto);
+        return "actualizarProducto";
+    }
+
+    @PostMapping("/{id}")
+    public String updateProducto(@PathVariable Long id, @ModelAttribute Producto detallesProducto) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontro el producto con el id: " + id));
+
         producto.setNombre(detallesProducto.getNombre());
+        producto.setDescripcion(detallesProducto.getDescripcion());
         producto.setPrecio(detallesProducto.getPrecio());
 
-        return productoRepository.save(producto);
+        productoRepository.save(producto);
+
+        return "redirect:/productos";
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteProducto(@PathVariable Long id) {
-        Producto producto = productoRepository.findById(id).orElseThrow(() -> new RuntimeException("No se encontro el producto con el id: " + id));;
+    @GetMapping("delete/{id}")
+    public String deleteProducto(Model model, @PathVariable Long id) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontró el producto con el id: " + id));
         productoRepository.delete(producto);
-        return "El producto con el id " + id + " se elimino";
+        return "redirect:/productos";
     }
 }
--- src/main/java/com/goga/apirest/apirest/Entities/Producto.java
+++ src/main/java/com/goga/apirest/apirest/Entities/Producto.java
@@ -11,26 +11,39 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    private String descripcion;
     private double precio;
-    
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public double getPrecio() {
         return precio;
     }
+
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
-    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
